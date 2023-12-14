f43e1560efd162960b3a35f90de035a547cb002f
niebzz <jaken108@gmail.com>
2023-12-14 11:23:26 -0500

    day 14 solution

---
+++ 2023/day 14/day14.py
@@ -0,0 +1,77 @@
+def push_rocks(row: list) -> list:
+    count_O = 0
+    for i, item in enumerate(row):
+        if item == "O":
+            count_O += 1
+            row[i] = "."
+        elif item == "#" and count_O > 0:
+            for j in range(i-count_O, i):
+                row[j] = "O"
+            count_O = 0
+
+        if i == len(row) - 1 and count_O > 0:
+            for k in range(len(row)-count_O, len(row)):
+                row[k] = "O"
+            count_O = 0
+    return row
+
+
+def transpose_platform_clockwise(data: list) -> list:
+    return list(map(lambda x: "".join(reversed(x)), zip(*data)))
+
+
+def transpose_platform_counterclockwise(data: list) -> list:
+    return ["".join(x) for x in reversed(list(zip(*data)))]
+
+
+def tilt_platform_east(data: list) -> list:
+    return ["".join(push_rocks(list(row))) for row in data]
+
+
+def tilt_platform_west(data: list) -> list:
+    return ["".join(reversed(x)) for x in (
+        tilt_platform_east(["".join(reversed(x)) for x in data]))]
+
+
+def tilt_platform_north(data: list) -> list:
+    return transpose_platform_counterclockwise(
+        tilt_platform_east(transpose_platform_clockwise(data)))
+
+
+def tilt_platform_south(data: list) -> list:
+    return transpose_platform_clockwise(
+        tilt_platform_east(transpose_platform_counterclockwise(data)))
+
+
+def calculate_load_north(data):
+    return sum([row.count("O") * (len(data) - i) for i, row in enumerate(data)])
+
+
+def spin_cycle(data):
+    return tilt_platform_east(tilt_platform_south(tilt_platform_west(tilt_platform_north(data))))
+
+
+def part1(data: list):
+    print(f"Part 1: {calculate_load_north(tilt_platform_north(data))}")
+
+
+def part2(data: list):
+    num_cycles = 1_000  # 1,000 ends up working out to be the same as 1 billion
+    i = 0
+    while i < num_cycles:
+        data = spin_cycle(data)
+        i += 1
+
+    print(f"Part 2: {calculate_load_north(data)}")
+
+
+def main():
+    with open(r"advent of code\2023\day 14\input.txt") as f:
+        platform = [line for line in f.read().strip().split("\n")]
+
+    part1(platform)
+    part2(platform)
+
+
+if __name__ == "__main__":
+    main()
