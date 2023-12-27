30ed71979627f18d172087af5ca3be2ca4352e0a
niebzz <jaken108@gmail.com>
2023-12-27 16:39:54 -0500

    2015 day 4

--- 2015/day 4/2015_4.py
+++ 2015/day 4/2015_4.py
@@ -1,13 +1,11 @@
 import hashlib
 
-INPUT = "yzbqklnj"
 
-
-def part1():
+def part1(input_string):
     i = 0
     first5 = 0
     while first5 != "00000":
-        str2hash = INPUT + str(i)
+        str2hash = input_string + str(i)
         md5 = hashlib.md5(str2hash.encode()).hexdigest()
         first5 = md5[:5]
         i += 1
@@ -15,11 +13,11 @@ def part1():
     print(f"Part 1: {i - 1}")
 
 
-def part2():
+def part2(input_string):
     i = 0
     first6 = 0
     while first6 != "000000":
-        str2hash = INPUT + str(i)
+        str2hash = input_string + str(i)
         md5 = hashlib.md5(str2hash.encode()).hexdigest()
         first6 = md5[:6]
         i += 1
@@ -28,5 +26,7 @@ def part2():
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    input_string = "yzbqklnj"
+
+    part1(input_string)
+    part2(input_string)
