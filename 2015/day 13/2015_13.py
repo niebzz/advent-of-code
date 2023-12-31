5995c0aadbed2ae72208e634d5c7487a0e057c78
niebzz <jaken108@gmail.com>
2023-12-30 19:11:42 -0500

    2015 day 13 p2

--- 2015/day 13/2015_13.py
+++ 2015/day 13/2015_13.py
@@ -62,12 +62,8 @@ def part2(data):
     return part1(data)
 
 
-def main():
+if __name__ == "__main__":
     data = get_puzzle_input(year=2015, day=13).read().split("\n")
 
     print(f"Part 1: {part1(data)}")
     print(f"Part 2: {part2(data)}")
-
-
-if __name__ == "__main__":
-    main()
