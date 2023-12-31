2f92845d90a663eb9f8fe012efb57c29d92a0391
niebzz <jaken108@gmail.com>
2023-12-30 19:00:58 -0500

    day 13 p2

--- 2015/day 13/2015_13.py
+++ 2015/day 13/2015_13.py
@@ -24,7 +24,6 @@ def get_permutations(names: tuple):
 
 def total_happiness(seating_arrangement: tuple, happiness_info: dict):
     total = 0
-    # print(seating_arrangement)
     for i, name in enumerate(seating_arrangement):
         left_name = seating_arrangement[i - 1]
         if i == len(seating_arrangement) - 1:
@@ -34,28 +33,12 @@ def total_happiness(seating_arrangement: tuple, happiness_info: dict):
         left_score = happiness_info[(name, left_name)]
         right_score = happiness_info[(name, right_name)]
         total += left_score + right_score
-        # print(name, (left_name, right_name), score)
     return total
 
 
 def part1():
-    data = get_puzzle_input(year=2015, day=13).read().split("\n")
-
-#     data = """Alice would gain 54 happiness units by sitting next to Bob.
-# Alice would lose 79 happiness units by sitting next to Carol.
-# Alice would lose 2 happiness units by sitting next to David.
-# Bob would gain 83 happiness units by sitting next to Alice.
-# Bob would lose 7 happiness units by sitting next to Carol.
-# Bob would lose 63 happiness units by sitting next to David.
-# Carol would lose 62 happiness units by sitting next to Alice.
-# Carol would gain 60 happiness units by sitting next to Bob.
-# Carol would gain 55 happiness units by sitting next to David.
-# David would gain 46 happiness units by sitting next to Alice.
-# David would lose 7 happiness units by sitting next to Bob.
-# David would gain 41 happiness units by sitting next to Carol.""".split("\n")
-
-    happiness_info = get_happiness_info(data)
     names = get_names(data)
+    happiness_info = get_happiness_info(data)
 
     max_score = 0
     for i, seating_arrangement in enumerate(get_permutations(names)):
@@ -63,8 +46,24 @@ def part1():
         if score > max_score:
             max_score = score
 
-    print(f"Part 1: {max_score}")
-    return happiness_info
+    return max_score
 
 
-part1()
+def part2():
+    def add_myself():
+        names = get_names(data)
+        me = "me"
+        for name in names:
+            data.append(
+                f"{me} would gain 0 happiness units by sitting next to {name}.")
+            data.append(
+                f"{name} would gain 0 happiness units by sitting next to {me}.")
+    add_myself()
+    return part1()
+
+
+# MAIN
+data = get_puzzle_input(year=2015, day=13).read().split("\n")
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
