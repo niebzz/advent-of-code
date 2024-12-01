06dac62cad13e967af2d41aecf068700e57914a7
Jake Niebauer <jaken108@gmail.com>
2024-12-01 09:37:21 -0500

    removed regex from solution

--- 2024/day01/day01.py
+++ 2024/day01/day01.py
@@ -1,16 +1,14 @@
-import re
-
 FILEPATH = r"advent_of_code\2024\day01\input.txt"
 
 with open(FILEPATH) as file:
     input = file.read().split("\n")
 
 # PART 1
-list1 = [int(y[0]) for y in [re.findall(r"\d+", x) for x in input]]
-list2 = [int(y[1]) for y in [re.findall(r"\d+", x) for x in input]]
+list1 = sorted([int(x.split()[0]) for x in input])
+list2 = sorted([int(x.split()[1]) for x in input])
 
 total_distance = 0
-for location1, location2 in zip(sorted(list1), sorted(list2)):
+for location1, location2 in zip(list1, list2):
     total_distance += abs(location2 - location1)
 
 print(f"Part 1: {total_distance}")
